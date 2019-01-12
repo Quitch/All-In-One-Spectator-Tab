947ba53925ccfa06aea92a7a7de78665383c0f60
Quitch <Quitch@users.noreply.github.com>
2019-01-12 21:52:35 +0000

    Make ALL the default tab

--- ui/mods/com.quitch.qallinonetab/live_game_players.js
+++ ui/mods/com.quitch.qallinonetab/live_game_players.js
@@ -1,24 +1,23 @@
 function allinonetab() {
+
+  // Setup tabs
+  model.spectatorPanelMode('all');
   model.showAllData = ko.computed(function () {
-    return model.spectatorPanelMode() == 'all';
+    return model.spectatorPanelMode() === 'all';
+  });
+  model.showAllData2 = ko.computed(function () {
+    return model.spectatorPanelMode() === 'all2';
   });
 
-  // All-in-one army count
-  model.spectatorPanelMode('all');
-
+  // ALL - All-in-one army count
   $('div.div_spectator_panel_buttons').append('<div class="div_spectator_panel_button" data-bind="click: function () { spectatorPanelMode(\'all\') }, css: { div_spectator_panel_button_active: showAllData } "><loc>ALL</loc></div>');
 
   $('table.tbl_spectator_stats tr:first').append(loadHtml('coui://ui/mods/com.quitch.qallinonetab/live_game_players1.html'));
 
   $('table.tbl_spectator_stats tr:last').append(loadHtml('coui://ui/mods/com.quitch.qallinonetab/live_game_players2.html'));
 
-  model.showAllData2 = ko.computed(function () {
-    return model.spectatorPanelMode() == 'all2';
-  });
 
-  // All-in-one army metal
-  model.spectatorPanelMode('all2');
-
+  // ALL2 - All-in-one army metal
   $('div.div_spectator_panel_buttons').append('<div class="div_spectator_panel_button" data-bind="click: function () { spectatorPanelMode(\'all2\') }, css: { div_spectator_panel_button_active: showAllData2 } "><loc>ALL2</loc></div>');
 
   $('table.tbl_spectator_stats tr:first').append(loadHtml('coui://ui/mods/com.quitch.qallinonetab/live_game_players3.html'));
