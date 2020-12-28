11218ac0111a9fd19bfba01ada5849d1d8908752
Quitch <Quitch@users.noreply.github.com>
2020-12-28 15:50:52 +0000

    Improve error catching code

--- ui/mods/com.quitch.qallinonetab/all_tabs.js
+++ ui/mods/com.quitch.qallinonetab/all_tabs.js
@@ -1,42 +1,45 @@
-function allInOneTab() {
-  // Setup tabs
-  model.spectatorPanelMode("all");
-  model.showAllData = ko.computed(function () {
-    return model.spectatorPanelMode() === "all";
-  });
-  model.showAllData2 = ko.computed(function () {
-    return model.spectatorPanelMode() === "all2";
-  });
+var allInOneSpectatorTab;
 
-  // ALL - All-in-one army count
-  $("div.div_spectator_panel_buttons").append(
-    '<div class="div_spectator_panel_button" data-bind="click: function () { spectatorPanelMode(\'all\') }, css: { div_spectator_panel_button_active: showAllData } "><loc>ALL</loc></div>'
-  );
+if (!allInOneSpectatorTab) {
+  allInOneSpectatorTab = true;
 
-  $("table.tbl_spectator_stats tr:first").append(
-    loadHtml("coui://ui/mods/com.quitch.qallinonetab/all_headers.html")
-  );
+  try {
+    // Setup tabs
+    model.spectatorPanelMode("all");
+    model.showAllData = ko.computed(function () {
+      return model.spectatorPanelMode() === "all";
+    });
+    model.showAllData2 = ko.computed(function () {
+      return model.spectatorPanelMode() === "all2";
+    });
 
-  $("table.tbl_spectator_stats tr:last").append(
-    loadHtml("coui://ui/mods/com.quitch.qallinonetab/all_values.html")
-  );
+    // ALL - All-in-one army count
+    $("div.div_spectator_panel_buttons").append(
+      '<div class="div_spectator_panel_button" data-bind="click: function () { spectatorPanelMode(\'all\') }, css: { div_spectator_panel_button_active: showAllData } "><loc>ALL</loc></div>'
+    );
 
-  // ALL2 - All-in-one army metal
-  $("div.div_spectator_panel_buttons").append(
-    '<div class="div_spectator_panel_button" data-bind="click: function () { spectatorPanelMode(\'all2\') }, css: { div_spectator_panel_button_active: showAllData2 } "><loc>ALL2</loc></div>'
-  );
+    $("table.tbl_spectator_stats tr:first").append(
+      loadHtml("coui://ui/mods/com.quitch.qallinonetab/all_headers.html")
+    );
 
-  $("table.tbl_spectator_stats tr:first").append(
-    loadHtml("coui://ui/mods/com.quitch.qallinonetab/all2_headers.html")
-  );
+    $("table.tbl_spectator_stats tr:last").append(
+      loadHtml("coui://ui/mods/com.quitch.qallinonetab/all_values.html")
+    );
 
-  $("table.tbl_spectator_stats tr:last").append(
-    loadHtml("coui://ui/mods/com.quitch.qallinonetab/all2_values.html")
-  );
-}
+    // ALL2 - All-in-one army metal
+    $("div.div_spectator_panel_buttons").append(
+      '<div class="div_spectator_panel_button" data-bind="click: function () { spectatorPanelMode(\'all2\') }, css: { div_spectator_panel_button_active: showAllData2 } "><loc>ALL2</loc></div>'
+    );
 
-try {
-  allInOneTab();
-} catch (e) {
-  console.error(e);
+    $("table.tbl_spectator_stats tr:first").append(
+      loadHtml("coui://ui/mods/com.quitch.qallinonetab/all2_headers.html")
+    );
+
+    $("table.tbl_spectator_stats tr:last").append(
+      loadHtml("coui://ui/mods/com.quitch.qallinonetab/all2_values.html")
+    );
+  } catch (e) {
+    console.error(e);
+    console.error(JSON.stringify(e));
+  }
 }
