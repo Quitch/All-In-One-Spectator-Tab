da9af22c11b9f68787170b7b30f403b509b198a4
Quitch <Quitch@users.noreply.github.com>
2022-07-24 21:27:38 +0100

    Select tab after assigning computed action

--- ui/mods/com.quitch.qallinonetab/all_tabs.js
+++ ui/mods/com.quitch.qallinonetab/all_tabs.js
@@ -5,7 +5,6 @@ if (!allInOneSpectatorTabLoaded) {
 
   function allInOneSpectatorTab() {
     try {
-      // ALL - All-in-one army count
       $("div.div_spectator_panel_buttons").append(
         loadHtml("coui://ui/mods/com.quitch.qallinonetab/all_buttons.html")
       );
@@ -17,8 +16,6 @@ if (!allInOneSpectatorTabLoaded) {
       $("table.tbl_spectator_stats tr:last").append(
         loadHtml("coui://ui/mods/com.quitch.qallinonetab/all_values.html")
       );
-
-      // ALL2 - All-in-one army metal
       $("div.div_spectator_panel_buttons").append(
         loadHtml("coui://ui/mods/com.quitch.qallinonetab/all2_buttons.html")
       );
@@ -31,8 +28,6 @@ if (!allInOneSpectatorTabLoaded) {
         loadHtml("coui://ui/mods/com.quitch.qallinonetab/all2_values.html")
       );
 
-      // Setup tabs
-      model.spectatorPanelMode("all");
       model.showAllData = ko.computed(function () {
         return model.spectatorPanelMode() === "all";
       });
@@ -40,6 +35,7 @@ if (!allInOneSpectatorTabLoaded) {
         return model.spectatorPanelMode() === "all2";
       });
 
+      model.spectatorPanelMode("all");
       model.pinSpectatorPanel(true);
     } catch (e) {
       console.error(e);
