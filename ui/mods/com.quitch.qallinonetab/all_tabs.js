03e675e0de1b765bb144ff791134bc00c5c0f810
Quitch <Quitch@users.noreply.github.com>
2024-05-23 16:44:32 +0100

    Reduce indentation

--- ui/mods/com.quitch.qallinonetab/all_tabs.js
+++ ui/mods/com.quitch.qallinonetab/all_tabs.js
@@ -1,41 +1,37 @@
 var allInOneSpectatorTabLoaded;
 
-if (!allInOneSpectatorTabLoaded) {
+function allInOneSpectatorTab() {
+  if (allInOneSpectatorTabLoaded) {
+    return;
+  }
+
   allInOneSpectatorTabLoaded = true;
 
-  function allInOneSpectatorTab() {
-    try {
-      var appendHtml = function (htmlClass, file) {
-        var path = "coui://ui/mods/com.quitch.qallinonetab/";
-        $(htmlClass).append(loadHtml(path + file));
-      };
+  try {
+    var appendHtml = function (htmlClass, file) {
+      var path = "coui://ui/mods/com.quitch.qallinonetab/";
+      $(htmlClass).append(loadHtml(path + file));
+    };
 
-      appendHtml("div.div_spectator_panel_buttons", "army_count_buttons.html");
-      appendHtml(
-        "table.tbl_spectator_stats tr:first",
-        "army_count_headers.html"
-      );
-      appendHtml("table.tbl_spectator_stats tr:last", "army_count_values.html");
-      appendHtml("div.div_spectator_panel_buttons", "army_metal_buttons.html");
-      appendHtml(
-        "table.tbl_spectator_stats tr:first",
-        "army_metal_headers.html"
-      );
-      appendHtml("table.tbl_spectator_stats tr:last", "army_metal_values.html");
+    appendHtml("div.div_spectator_panel_buttons", "army_count_buttons.html");
+    appendHtml("table.tbl_spectator_stats tr:first", "army_count_headers.html");
+    appendHtml("table.tbl_spectator_stats tr:last", "army_count_values.html");
+    appendHtml("div.div_spectator_panel_buttons", "army_metal_buttons.html");
+    appendHtml("table.tbl_spectator_stats tr:first", "army_metal_headers.html");
+    appendHtml("table.tbl_spectator_stats tr:last", "army_metal_values.html");
 
-      model.showArmyCount = ko.computed(function () {
-        return model.spectatorPanelMode() === "armyCount";
-      });
-      model.showArmyMetal = ko.computed(function () {
-        return model.spectatorPanelMode() === "armyMetal";
-      });
+    model.showArmyCount = ko.computed(function () {
+      return model.spectatorPanelMode() === "armyCount";
+    });
+    model.showArmyMetal = ko.computed(function () {
+      return model.spectatorPanelMode() === "armyMetal";
+    });
 
-      model.spectatorPanelMode("armyCount");
-      model.pinSpectatorPanel(true);
-    } catch (e) {
-      console.error(e);
-      console.error(JSON.stringify(e));
-    }
+    model.spectatorPanelMode("armyCount");
+    model.pinSpectatorPanel(true);
+  } catch (e) {
+    console.error(e);
+    console.error(JSON.stringify(e));
   }
-  allInOneSpectatorTab();
 }
+allInOneSpectatorTab();
