74b562d3102a3442bdf081e1ff7614cbc603a592
Quitch <Quitch@users.noreply.github.com>
2026-08-03 09:04:50 +0100

    Update error reporting to always have a string

--- ui/mods/com.quitch.qallinonetab/all_tabs.js
+++ ui/mods/com.quitch.qallinonetab/all_tabs.js
@@ -31,7 +31,7 @@ function allInOneSpectatorTab() {
     model.pinSpectatorPanel(true);
   } catch (e) {
     console.error(e);
-    console.error(JSON.stringify(e));
+    console.error("All-In-One Spectator Tab: " + (e.stack || e.message || e));
   }
 }
 allInOneSpectatorTab();
